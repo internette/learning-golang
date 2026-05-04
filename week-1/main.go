cb35df5eb3f8a4d33d16bc0bfbb67b16fde988a2
Antoinette Janus <antoinettejanus@Antoinettes-Laptop.local>
2026-05-04 11:01:01 -0700

    Writes task to file

--- week-1/main.go
+++ week-1/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func addTask(taskName string) {
-	fmt.Printf("- %s\n", taskName)
+	var file, err = os.Create("example.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	// Write a string to the file
+	_, err = file.WriteString(taskName)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("Data written successfully.")
 }
 
 func main() {
